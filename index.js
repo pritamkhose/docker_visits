9a8e0883e0c45ca6885784bb7130f08bc12ab594
Pritam Khose <pritamkhose@gmail.com>
2022-09-13 15:16:45 +0530

    URI update

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const redis = require("redis");
 
 const client = redis.createClient({
-  host: "redis-server",
-  port: 6379,
+  // host: "redis-server",
+  // port: 6379,
+  url: 'redis://redis-server:6379',
 });
 client.connect();
 client.on("connect", () => console.log("Connected to redis 6379"));
@@ -19,6 +20,14 @@ app.get("/", async (req, res) => {
     "Hi, Time is " + new Date().toISOString() + ". No of visit : " + visits
   );
   client.set("visits", parseInt(visits) + 1);
+});
+
+app.get("/env", (req, res) => {
+  res.send(process.env);
+});
+
+app.get("/stop", (req, res) => {
+  process.exit(0);
 });
 
 app.listen(8080, () => {
